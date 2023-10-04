027cdd369c3b5c822728a58d6ad1d52929b15df5
saeed <saeed00014@gmail.com>
2023-10-04 18:03:54 +0330

    fix server

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 require("dotenv").config();
 const bodyParser = require('body-parser')
-const mongoose = require("mongoose");
+]
 const express = require("express");
 const path = require("path");
 const cors = require('cors')
 const app = express();
 
-const router = require("./backend/route/product");
-
 app.use(cors())
 
 app.use(bodyParser.json())
-
-app.use('/products', router);
 
 app.use("/", express.static(path.join(__dirname, "./dist")));
 
@@ -21,12 +17,3 @@ app.get("/*", (_req, res) => {
 })
 
 app.listen(4000, () => console.log("Server is running"));
-
-mongoose.connect(
-  'mongodb+srv://saeed00014:amiralim890@cluster0.79q6syx.mongodb.net/?retryWrites=true&w=majority',
-  {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      writeConcern: { w: 'majority' },
-  }
-);
